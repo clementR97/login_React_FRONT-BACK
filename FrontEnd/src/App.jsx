2d2f6eb1eede0d0501ae65a1cbdf369edefe8d4d
clement Roland <clementroland52@gmail.com>
2026-02-04 13:05:03 -0400

    Enhance: add Bootstrap 5.3.8 to dependencies and import styles in App component

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-
+import 'bootstrap/dist/css/bootstrap.css';
 import viteLogo from '/vite.svg'
 import './App.css'
 
--- FrontEnd/src/Components/login.jsx
+++ FrontEnd/src/Components/login.jsx
@@ -1,3 +1,9 @@
+import {MDBBtn,
+    MDBContainer,
+    MDBRow,
+    MDBCol,
+    MDBInput}
+    from ''
 
 const login = ()=>{
 
