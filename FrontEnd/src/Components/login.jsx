19fc83369cd01a59d9149eea049ca0a5b0ae0a6e
clement Roland <clementroland52@gmail.com>
2026-02-05 13:05:07 -0400

    Enhance: integrate Google and Facebook sign-in options, and include Forgot Password functionality in the login component

--- FrontEnd/src/Components/login.jsx
+++ FrontEnd/src/Components/login.jsx
@@ -13,6 +13,9 @@ import Typography from '@mui/material/Typography';
 import Stack from '@mui/material/Stack';
 import MuiCard from '@mui/material/Card';
 import { styled } from '@mui/material/styles';
+import { GoogleIcon,FacebookIcon,SitemarkIcon } from './CustomIcon.jsx';
+
+import ForgotPassword from './ForgotPassword.jsx';
 
 const Card = styled(MuiCard)(({theme})=>({
     display:'flex',
@@ -26,11 +29,11 @@ const Card = styled(MuiCard)(({theme})=>({
         maxWidth:'450px',
     },
     boxShadow:
-    'hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px',
-    ...theme.applyStyles('dark', {
-      boxShadow:
-        'hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px',
-    }),
+    'hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px',
+    // ...theme.applyStyles('dark', {
+    //   boxShadow:
+    //     'hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px',
+    // }),
 }));
 
 const SignInContainer = styled(Stack)(({theme})=>({
@@ -47,12 +50,12 @@ const SignInContainer = styled(Stack)(({theme})=>({
       zIndex: -1,
       inset: 0,
       backgroundImage:
-        'radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))',
-      backgroundRepeat: 'no-repeat',
-      ...theme.applyStyles('dark', {
-        backgroundImage:
-          'radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))',
-      }),
+        'radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))',
+    //   backgroundRepeat: 'no-repeat',
+    //   ...theme.applyStyles('dark', {
+    //     backgroundImage:
+    //       'radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))',
+    //   }),
     },
 }));
 
@@ -117,7 +120,7 @@ const validateInputs = ()=>{
         <SignInContainer direction="column" justifyContent="space-between">
             {/* <ColorModeSelect sx={{position: 'fixed',top:'1rem',right:'1rem'}}/> */}
             <Card cariant="outlined">
-                {/* logo <SitmarkIcon/> */}
+                 <SitemarkIcon/>
                 <Typography component="h1" variant="h4" sx={{width: '100%', fontSize: 'clamp(2rem, 10vw, 2.15rem)'}}>
                     Sign in
                 </Typography>
@@ -165,7 +168,7 @@ const validateInputs = ()=>{
                         control={<Checkbox value='remember' color='primary'/>}
                         label='Remember me'
                         />
-                        {/* <ForgotPassword open={open} handleClose={handleClose}/> */}
+                        <ForgotPassword open={open} handleClose={handleClose}/>
                         <Button
                         type='submit'
                         fullWidth
@@ -188,7 +191,7 @@ const validateInputs = ()=>{
                     fullWidth
                     variant='outlined'
                     onClick={()=> alert('Sign in with Google')}
-                    // startIcon={<GoogleIcon/>}
+                     startIcon={<GoogleIcon/>}
                     >
                         Sign in with Google
                     </Button>
@@ -196,7 +199,7 @@ const validateInputs = ()=>{
                         fullWidth
                         variant="outlined"
                         onClick={() => alert('Sign in with Facebook')}
-                        //   startIcon={<FacebookIcon />}
+                           startIcon={<FacebookIcon />}
                         >
                         Sign in with Facebook
                         </Button>
