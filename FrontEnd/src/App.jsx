96febfd21ed4a7df91dc9621c201272f1509bc2f
clement Roland <clementroland52@gmail.com>
2026-02-06 14:44:11 -0400

    Enhance: add react-router-dom for routing and implement basic navigation structure in App component

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import Login from './Components/login.jsx'
-
+import SignUp from './Components/SignUp.jsx'
+import Accueil from './Components/Accueil.jsx'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import './App.css'
 
 function App() {
@@ -8,9 +10,16 @@ function App() {
 
   return (
     
-          <>
-          <Login/>
-          </>
+          <BrowserRouter>
+            <Routes>
+              <Route path='/' element={<Accueil/>}/>
+              <Route path='/Sign-in' element={<Login/>}/>
+              <Route path='/Sign-up' element={<SignUp/>}/>
+              {/* <Route path='*' element={<PageNotFound/>}/> */}
+            </Routes>
+          {/* <Login/> */}
+          {/* <SignUp/> */}
+          </BrowserRouter>
     
   )
 }
