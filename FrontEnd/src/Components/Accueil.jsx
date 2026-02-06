9d55d7205db6931b2baf8cec45721fc8cc2b3d7c
clement Roland <clementroland52@gmail.com>
2026-02-06 16:01:46 -0400

    Enhance: implement Material-UI components in Accueil for improved layout and styling

--- FrontEnd/src/Components/Accueil.jsx
+++ FrontEnd/src/Components/Accueil.jsx
@@ -1,5 +1,12 @@
-
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import NavBar from './ComponentAccueil/Navbar.jsx';
 const Accueil = ()=>{
-
+ return(
+    <>
+    <CssBaseline enableColorScheme />
+    <NavBar/>
+    </>
+ )
 }
 export default Accueil
