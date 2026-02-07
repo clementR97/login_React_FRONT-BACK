663cb634b1305798180ecab6f6224ce1046054e1
clement Roland <clementroland52@gmail.com>
2026-02-07 14:10:18 -0400

    Enhance: integrate Hero and Footer components into Accueil for improved layout and user experience

--- FrontEnd/src/Components/Accueil.jsx
+++ FrontEnd/src/Components/Accueil.jsx
@@ -1,11 +1,15 @@
 import CssBaseline from '@mui/material/CssBaseline';
 import Divider from '@mui/material/Divider';
 import NavBar from './ComponentAccueil/Navbar.jsx';
+import Hero from './ComponentAccueil/Hero.jsx';
+import Footer from './ComponentAccueil/Footer.jsx';
 const Accueil = ()=>{
  return(
     <>
     <CssBaseline enableColorScheme />
     <NavBar/>
+    <Hero/>
+    <Footer/>
     </>
  )
 }
