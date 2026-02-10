fed9a4583c1077c6b3b4fc343a6794adb341f4c1
clement Roland <clementroland52@gmail.com>
2026-02-10 12:51:43 -0400

    Enhance: integrate authentication context and secure dashboard route in App component using react-router-dom

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,25 +1,35 @@
-import { useState } from 'react'
+import {BrowserRouter,Routes,Route,Navigate} from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext.jsx'
 import Login from './Components/login.jsx'
 import SignUp from './Components/SignUp.jsx'
+import Dashboard from'./Components/Dashboard.jsx'
 import Accueil from './Components/Accueil.jsx'
-import {BrowserRouter,Routes,Route,Navigate} from 'react-router-dom'
+import ProtectedRoute from './Components/ProtectedRoute.jsx'
 import './App.css'
 
 function App() {
   
 
   return (
-    
+          <AuthProvider>
           <BrowserRouter>
             <Routes>
               <Route path='/' element={<Accueil/>}/>
               <Route path='/Sign-in' element={<Login/>}/>
               <Route path='/Sign-up' element={<SignUp/>}/>
+              {/* Secure Routes */}
+              <Route
+              path='dashboard'
+              element={
+                <ProtectedRoute>
+                  <Dashboard/>
+                </ProtectedRoute>
+              }/>
               <Route path='*' element={<Navigate to="/" replace />} />
             </Routes>
           
           </BrowserRouter>
-    
+          </AuthProvider>
   )
 }
 
